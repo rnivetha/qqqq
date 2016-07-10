bd4af1991db538c9436ab810adacf190f5b2b926
rnivetha <rnivetha1995@gmail.com>
2016-07-10 12:58:43 +0530

    finish

---
+++ ttttyt.c
@@ -0,0 +1,31 @@
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+char name={"TREE"};
+int thick;
+int branch;
+char color[];
+int angle;
+printf("enter the thickness of the tree");
+scanf("%d,&thick of the tree");
+printf("enter the branches of the tree");
+scanf("%d,branch of the tree");
+printf("enter the color of the tree");
+scanf("%c,&color of the tree");
+printf("enter the angle of the tree");
+scanf("%d,&angle of the tree");
+if(thick>23)
+{
+if (branch>20)
+{
+if(angle<=45)
+{
+printf(" the angle of the tree is:");
+}
+printf("the branches of the tree is:");
+}
+printf("\n the thickness of the tree is:");
+}
+getch()
+}
